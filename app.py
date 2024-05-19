d9d53ef5a4a8c413785f56f2cfad048b83e1c80b
PedroOPozo96 <168337714+PedroOPozo96@users.noreply.github.com>
2024-05-19 21:53:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-app.run("0.0.0.0",30000,debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
